a451998cfdabc102c9712c08265cdff8b2a01ae0
artur <artur@DESKTOP-V0JU5PP>
2022-10-09 22:57:17 -0300

    Add project files.

---
+++ Adocao_ONG_V2.0/Program.cs
@@ -0,0 +1,114 @@
+﻿using Controllers;
+using System;
+
+namespace Adocao_ONG_V2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMenu();
+        }
+
+        static void MostrarMenu()
+        {
+            int opcao = 10;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" |===============  MENU  ===============|");
+                Console.WriteLine(" |Opção 1 : Cadastrar Pessoa            |");
+                Console.WriteLine(" |Opção 2 : Alterar Dados Pessoa        |");
+                Console.WriteLine(" |Opção 3 : Mostrar Pessoas Cadastradas |");
+                Console.WriteLine(" |======================================|");
+                Console.WriteLine(" |Opção 4 : Cadastrar Animal            |");
+                Console.WriteLine(" |Opção 5 : Alterar Dados Animal        |");
+                Console.WriteLine(" |Opção 6 : Mostrar Animais Cadastrados |");
+                Console.WriteLine(" |======================================|");
+                Console.WriteLine(" |Opção 7 : Adotar um Animal            |");
+                Console.WriteLine(" |Opção 8 : Mostrar Adoções             |");
+                Console.WriteLine(" |Opção 9 : Mostrar Animais Disponiveis |");
+                Console.WriteLine(" |======================================|");
+                Console.WriteLine(" |Opção 0 : Sair                        |");
+                Console.WriteLine(" |======================================|");
+
+                Console.Write("\n Informe a opção: ");
+
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                }
+
+                switch (opcao)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+
+                    case 1:
+                        Console.Clear();
+                        new ONGController().AdicionarPessoa();
+                        MostrarMenu();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        new ONGController().AtualizarPessoa();
+                        MostrarMenu();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        new ONGController().SelecionarPessoa();
+                        MostrarMenu();
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        new ONGController().AdicionarAnimal();
+                        MostrarMenu();
+                        break;
+
+                    case 5:
+                        Console.Clear();
+                        new ONGController().AtualizarAnimal();
+                        MostrarMenu();
+                        break;
+
+                    case 6:
+                        Console.Clear();
+                        new ONGController().SelecionarAnimal();
+                        MostrarMenu();
+                        break;
+
+                    case 7:
+                        Console.Clear();
+                        new ONGController().Adotar();
+                        MostrarMenu();
+                        break;
+
+                    case 8:
+                        Console.Clear();
+                        new ONGController().SelecionarAdotados();
+                        MostrarMenu();
+                        break;
+
+                    case 9:
+                        Console.Clear();
+                        new ONGController().SelecionarDisponiveis();
+                        MostrarMenu();
+                        break;
+
+                    default:
+                        Console.Write("\n Opcao Inválida! Aperte [ENTER] para executar novamente.");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Controllers/ONGController.cs
@@ -0,0 +1,53 @@
+﻿using Models;
+using Services;
+
+namespace Controllers
+{
+    public class ONGController
+    {
+        public Pessoa AdicionarPessoa()
+        {
+            return new ONGServices().InserirPessoaBanco();
+        }
+
+        public Pessoa SelecionarPessoa()
+        {
+            return new ONGServices().SelecionarPessoasBanco();
+        }
+
+        public Pessoa AtualizarPessoa()
+        {
+            return new ONGServices().AtualizarPessoaBanco();
+        }
+
+        public Animal AdicionarAnimal()
+        {
+            return new ONGServices().InserirAnimalBanco();
+        }
+
+        public Animal SelecionarAnimal()
+        {
+            return new ONGServices().SelecionarAnimaisBanco();
+        }
+
+        public Animal AtualizarAnimal()
+        {
+            return new ONGServices().AtualizarAnimalBanco();
+        }
+
+        public Pessoa Adotar()
+        {
+            return new ONGServices().AdotarAnimal();
+        }
+
+        public Animal SelecionarAdotados()
+        {
+            return new ONGServices().SelecionarAnimaisAdotadosBanco();
+        }
+
+        public Animal SelecionarDisponiveis()
+        {
+            return new ONGServices().SelecionarAnimaisDisponiveisBanco();
+        }
+    }
+}
